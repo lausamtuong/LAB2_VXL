7a33cad16fac81fe0ed8c1826b3d2593e261c475
lausamtuong <lausamtuong@gmail.com>
2022-09-28 06:52:07 +0700

    ex1

---
+++ EX1/Core/Inc/BUTTON.h
@@ -0,0 +1,15 @@
+/*
+ * BUTTON.h
+ *
+ *  Created on: Sep 24, 2022
+ *      Author: Tam Thanh Tin
+ */
+#include "main.h"
+#ifndef INC_BUTTON_H_
+#define INC_BUTTON_H_
+
+
+#define NORMAL_STATE SET
+#define PRESSED_STATE RESET
+void getKeyInput();
+#endif /* SRC_BUTTON_H_ */
